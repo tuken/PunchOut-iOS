b8467f9a4aa092cf2fc9a2a333e92a77b50d4d46
secual-ikawa <tsukasa.ikawa@secual-inc.com>
2022-03-21 23:38:58 +0900

    [add] 雛形

---
+++ Shared/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 井川 司 on 2022/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        
+        ContentView()
+    }
+}
---
+++ Shared/PunchOutApp.swift
@@ -0,0 +1,20 @@
+//
+//  PunchOutApp.swift
+//  Shared
+//
+//  Created by 井川 司 on 2022/03/21.
+//
+
+import SwiftUI
+
+@main
+struct PunchOutApp: App {
+    
+    var body: some Scene {
+        
+        WindowGroup {
+            
+            ContentView()
+        }
+    }
+}
