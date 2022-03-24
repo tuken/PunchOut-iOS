f55b3a2e7bf81487ad015897c19e8e3339ca5dfc
secual-ikawa <tsukasa.ikawa@secual-inc.com>
2022-03-24 17:41:31 +0900

    [update] MainView
    MenuViewの表示方法調整

--- Shared/MainView.swift
+++ Shared/MainView.swift
@@ -10,48 +10,52 @@ import SwiftUI
 struct MainView: View {
     
     @State var showMenu = false
-
+    
     var body: some View {
         
-        let drag = DragGesture()
-            .onEnded {
-                if $0.translation.width < -100 {
-                    withAnimation {
-                        self.showMenu = false
-                    }
-                }
-            }
-        
-        return NavigationView {
-            GeometryReader { geometry in
-                ZStack(alignment: .leading) {
+        ZStack {
+            
+            NavigationView {
+                
+                GeometryReader { geometry in
+                    
                     HomeView(showMenu: self.$showMenu)
                         .frame(width: geometry.size.width, height: geometry.size.height)
-//                        .offset(x: self.showMenu ? geometry.size.width/2 : 0)
+                        //                        .offset(x: self.showMenu ? geometry.size.width/2 : 0)
                         .disabled(self.showMenu ? true : false)
                 }
-                .gesture(drag)
-                
-                if self.showMenu {
+                .navigationBarTitle("", displayMode: .inline)
+                .navigationBarItems(leading: (
+                    Button(action: {
+                        withAnimation {
+                            self.showMenu.toggle()
+                        }
+                    }) {
+                        Image(systemName: "line.horizontal.3")
+                            .imageScale(.large)
+                            .foregroundColor(.white)
+                    }
+                ))
+                .background(Color(red: 1/255, green: 172/255, blue: 200/255))
+                .navigationBarColor(Color(red: 1/255, green: 172/255, blue: 200/255))
+            }
+            
+            if self.showMenu {
+                GeometryReader { geometry in
+                    
                     MenuView()
                         .frame(width: geometry.size.width*4/5, height: geometry.size.height)
                         .transition(.move(edge: .leading))
+                        .gesture(DragGesture()
+                                    .onEnded {
+                                        if $0.translation.width < -100 {
+                                            withAnimation {
+                                                self.showMenu = false
+                                            }
+                                        }
+                                    })
                 }
             }
-            .navigationBarTitle("", displayMode: .inline)
-            .navigationBarItems(leading: (
-                Button(action: {
-                    withAnimation {
-                        self.showMenu.toggle()
-                    }
-                }) {
-                    Image(systemName: "line.horizontal.3")
-                        .imageScale(.large)
-                        .foregroundColor(.white)
-                }
-            ))
-            .background(Color(red: 1/255, green: 172/255, blue: 200/255))
-            .navigationBarColor(Color(red: 1/255, green: 172/255, blue: 200/255))
         }
     }
 }
@@ -59,30 +63,30 @@ struct MainView: View {
 struct NavigationBarModifier: ViewModifier {
     
     let backgroundColor: Color
-
+    
     init(backgroundColor: Color) {
         
         self.backgroundColor = backgroundColor
-
+        
         let coloredAppearance = UINavigationBarAppearance()
         coloredAppearance.configureWithTransparentBackground()
         coloredAppearance.backgroundColor = UIColor(backgroundColor)
         coloredAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
         coloredAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-
+        
         UINavigationBar.appearance().standardAppearance = coloredAppearance
         UINavigationBar.appearance().compactAppearance = coloredAppearance
         UINavigationBar.appearance().scrollEdgeAppearance = coloredAppearance
         UINavigationBar.appearance().tintColor = .white
     }
-
+    
     func body(content: Content) -> some View {
         content
     }
 }
 
 extension View {
-
+    
     func navigationBarColor(_ backgroundColor: Color) -> some View {
         
         modifier(NavigationBarModifier(backgroundColor: backgroundColor))
